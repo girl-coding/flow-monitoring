efebbe96d5a48462e9974edda6400c1cd5b48190
CHAOUKI Mouad <chaouki.mouad.pro@gmail.com>
2023-07-05 19:37:30 +0200

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { COMPONENTS_MATERIALS } from './component.const';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    COMPONENTS_MATERIALS,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
