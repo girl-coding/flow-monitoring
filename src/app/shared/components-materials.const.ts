1be5f9e12456efecbaa7521b74354cdc3e525a43
CHAOUKI Mouad <chaouki.mouad.pro@gmail.com>
2023-07-06 21:09:29 +0200

    add styles

---
+++ src/app/shared/components-materials.const.ts
@@ -0,0 +1,3 @@
+import { MatIconModule } from '@angular/material/icon';
+
+export const COMPONENTS_MATERIALS = [MatIconModule];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { COMPONENTS } from './components/components.const';
+import { COMPONENTS_MATERIALS } from './components-materials.const';
 
 @NgModule({
   declarations: [COMPONENTS],
   exports: [COMPONENTS],
-  imports: [CommonModule],
+  imports: [CommonModule, COMPONENTS_MATERIALS],
 })
 export class SharedModule {}
