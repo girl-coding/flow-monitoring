702f153a8757f6c181d9346fe99df9a4567f2967
CHAOUKI Mouad <chaouki.mouad.pro@gmail.com>
2023-07-13 15:28:39 +0200

    Add template for dashboard

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { COMPONENTS_MATERIALS } from './components-materials.const';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeHeaderComponent],
+  declarations: [HomeComponent, HomeHeaderComponent, HomeContentComponent],
   imports: [HomeRoutingModule, SharedModule, COMPONENTS_MATERIALS],
 })
 export class HomeModule {}
